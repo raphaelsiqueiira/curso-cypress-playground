c52c0f1103d610be624809baab2b90d5675d0df6
Raphael Borba <raphaelborba10@gmail.com>
2025-10-01 08:06:00 -0300

    Adicionando arquivos para CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2m2j7o",
   e2e: {
     setupNodeEvents(on, config) {
       // eventos ou plugins podem ser configurados aqui
